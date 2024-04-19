a516ecf7d1385dfc242e3d5c7bc466d493be7a76
Cristianoaf81GIT <cristianoaf81@gmail.com>
2024-04-19 07:50:23 -0300

    feat(add search bar): install react native paper
    add material design
    add search bar component
    
    Closes #main

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import { StatusBar as ExporStatusBar } from 'expo-status-bar';
 import {
   StyleSheet, Text, View, SafeAreaView, Platform, StatusBar as Bar,
 } from 'react-native';
+import { Searchbar, MD2Colors } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,8 +12,11 @@ const styles = StyleSheet.create({
   },
   search: {
     padding: 16,
-    backgroundColor: 'green',
+    backgroundColor: MD2Colors.grey200,
     marginTop: Platform.OS === 'android' ? Bar.currentHeight + 1 : 0,
+  },
+  searchBar: {
+    backgroundColor: MD2Colors.grey50,
   },
   list: { flex: 1, padding: 16, backgroundColor: 'blue' },
 });
@@ -21,12 +25,26 @@ const StatusBarStyles = {
   AUTO: 'auto',
 };
 
+const SearchBarTheme = {
+  colors: {
+    primary: MD2Colors.blue500,
+  },
+};
+
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>Busca:</Text>
+          <Searchbar
+            placeholder="Buscar"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
+            theme={SearchBarTheme.colors}
+            style={styles.searchBar}
+          />
         </View>
         <View style={styles.list}>
           <Text>Lista:</Text>
