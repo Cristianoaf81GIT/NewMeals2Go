42020123409de66462502254fc919b8002397d06
cristianoaf81 <cristianoaf81@gmail.com>
2024-07-06 16:45:23 -0300

    feat(add rating): use svg for rating
    Closes #main

--- App.jsx
+++ App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { StatusBar as ExporStatusBar } from 'expo-status-bar';
+import React from "react";
+import { StatusBar as ExporStatusBar } from "expo-status-bar";
 // eslint-disable-next-line camelcase
-import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
+import { useFonts, Lato_400Regular } from "@expo-google-fonts/lato";
 // eslint-disable-next-line camelcase
-import { Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { Oswald_400Regular } from "@expo-google-fonts/oswald";
 
-import { ThemeProvider } from 'styled-components/native';
+import { ThemeProvider } from "styled-components/native";
 
-import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
-import { theme } from './src/infrastruture/theme';
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infrastruture/theme";
 
 const StatusBarStyles = {
-  AUTO: 'auto',
+  AUTO: "auto"
 };
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    // eslint-disable-next-line camelcase 
+    // eslint-disable-next-line camelcase
     Oswald_400Regular,
     // eslint-disable-next-line camelcase
-    Lato_400Regular,
+    Lato_400Regular
   });
 
   if (!fontsLoaded) {
--- src/features/restaurants/components/restaurant-info-card.jsx
+++ src/features/restaurants/components/restaurant-info-card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Text as PaperText, MD2Colors } from "react-native-paper";
 import { styled } from "styled-components/native";
+import Star from "../../../assets/star.svg";
 
 const RestaurantCard = styled(Card).attrs({ mode: "elevated", elevation: 5 })`
   background-color: ${MD2Colors.grey200};
@@ -11,9 +12,24 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled(PaperText).attrs({ variant: "bodyMedium" })`
-  padding: 16px;
   color: ${MD2Colors.red200};
-  font-family: ${(props) => props.theme.fonts.body}
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled(PaperText)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  paddind-bottom: ${(props) => props.theme.space[2]};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -27,10 +43,20 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily
   } = restaurant;
 
+  const ratingArray = (rating) => Array.from(new Array(Math.ceil(rating)));
+
   return (
     <RestaurantCard>
       <RestaurantCardCover source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArray(rating).map((_) => (
+            <Star width={20} height={20} key={_} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
