8ced283fc66d606285f98b28277a7bfc0f0490de
cristianoaf81 <cristianoaf81@gmail.com>
2024-07-16 17:55:32 -0300

    feat(restaurant-info-card): finishing restaurant-info-card
    Closes #main

--- src/features/restaurants/components/restaurant-info-card.jsx
+++ src/features/restaurants/components/restaurant-info-card.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Card, Text as PaperText, MD2Colors } from "react-native-paper";
 import { styled } from "styled-components/native";
 import Star from "../../../assets/star.svg";
+import OpenIcon from "../../../assets/open.svg";
+import uuid from "react-native-uuid";
 
 const RestaurantCard = styled(Card).attrs({ mode: "elevated", elevation: 5 })`
   background-color: ${MD2Colors.grey200};
@@ -12,7 +14,7 @@ const RestaurantCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled(PaperText).attrs({ variant: "bodyMedium" })`
-  color: ${MD2Colors.red200};
+  color: ${MD2Colors.red600};
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.body};
 `;
@@ -32,15 +34,31 @@ const Rating = styled.View`
   paddind-bottom: ${(props) => props.theme.space[2]};
 `;
 
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const ClosedTemporarily = styled(PaperText).attrs({ variant: "labelSmall" })`
+  color: ${MD2Colors.red400};
+  margin-right: ${(props) => props.theme.sizes[1]};
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some restaurant",
     icon,
-    photos = ["https://www.melhoresdestinos.com.br/wp-content/uploads/2023/06/restaurante-fazenda-sao-paulo-capa.jpg"],
+    photos = ["https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia27698/culinaria-mineira-cpt.jpg"],
     address = "100 Some random address",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily
+    isClosedTemporarily = true
   } = restaurant;
 
   const ratingArray = (rating) => Array.from(new Array(Math.ceil(rating)));
@@ -50,11 +68,17 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCardCover source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray(rating).map((_) => (
-            <Star width={20} height={20} key={_} />
-          ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray(rating).map((_) => (
+              <Star width={20} height={20} key={`${_}${uuid.v4()}`} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && <ClosedTemporarily>CLOSED TEMPORARILY</ClosedTemporarily>}
+            {isOpenNow && <OpenIcon width={20} height={20} />}
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
