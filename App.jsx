98264798ac3004ebd7c1d4f777077054e424033d
Cristiano Alexandre de Faria <cristianoaf81@gmail.com>
2024-10-26 14:53:16 -0300

    feat(144): fake an api request
    Closes#main

--- App.jsx
+++ App.jsx
@@ -18,6 +18,10 @@ import { theme } from './src/infrastruture/theme';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { SafeContainer } from './src/components/safe-area/safe-area.component';
 
+import { restaurantRequest } from "./src/services/restaurants/restaurant.service";
+
+restaurantRequest().then(d => console.log(JSON.stringify(d, null, 4))).catch(console.error);
+
 const StatusBarStyles = {
   AUTO: 'auto',
 };
